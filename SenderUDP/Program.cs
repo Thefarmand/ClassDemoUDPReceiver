cc1d46af0bb9314eb8025e3aa7c133f660d29c73
Peter Levinsky <pele@easj.dk>
2018-11-09 11:07:37 +0100

    Add project files.

---
+++ SenderUDP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SenderUDP
+{
+    class Program
+    {
+        private const int PORT = 7;
+
+        static void Main(string[] args)
+        {
+            Sender sender = new Sender(PORT);
+            sender.start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SenderUDP/Sender.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SenderUDP
+{
+    internal class Sender
+    {
+        private int PORT;
+
+        public Sender(int port)
+        {
+            this.PORT = port;
+        }
+
+        public void start()
+        {
+            String navn = "Peter";
+
+            byte[] data = Encoding.ASCII.GetBytes(navn);
+            IPEndPoint receiverEP = new IPEndPoint(IPAddress.Broadcast, PORT);
+
+            using (UdpClient senderSock = new UdpClient()) // ingen port = lytter IKKE
+            {
+                senderSock.EnableBroadcast = true;
+
+                senderSock.Send(data, data.Length, receiverEP);
+
+                IPEndPoint FromReceiverEP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] inData = senderSock.Receive(ref FromReceiverEP);
+
+                String inStr = Encoding.ASCII.GetString(inData);
+
+                Console.WriteLine("Modtaget = " + inStr);
+
+            }
+        }
+    }
+}
